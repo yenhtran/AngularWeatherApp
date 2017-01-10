ba14a5281df46a68584015663455a911afaac37d
Yen Tran <yen.tran@autodesk.com>
2017-01-10 14:54:56 -0800

    fixes routing problem

--- app.js
+++ app.js
@@ -2,26 +2,36 @@
 var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
 
 //ROUTES
-weatherApp.config(function($routeProvider) {
-	$routeProvider
+weatherApp.config(function ($routeProvider) {
+   
+    $routeProvider
+    
+    .when('/', {
+        templateUrl: 'pages/home.html',
+        controller: 'homeController'
+    })
+    
+    .when('/forecast', {
+        templateUrl: 'pages/forecast.html',
+        controller: 'forecastController'
+    })
+    
+});
 
-	.when('/', {
-		templateUrl: 'pages/home.html',
-		controller: 'homeController'
-	})
-
-	.when('/forecast', {
-		templateUrl: 'pages/forecast.html',
-		controller: 'forecastController'
-	})
-
+//SERVICES
+weatherApp.service('cityService', function(){
+    this.city = 'San Francisco, CA';
 });
 
 //CONTROLLER
-weatherApp.controller('homeController', ['$scope', '$log', function($scope, $log){
-	$scope.name = 'Home';
+weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService){
+	$scope.city = cityService.city;
+    $scope.$watch('city', function(){
+        cityService.city = $scope.city;
+    });                                                                           
 }]);
 
-weatherApp.controller('forecastController', ['$scope', function($scope, $log){
-	$scope.name = 'Forecast';
+weatherApp.controller('forecastController', ['$scope', 'cityService', function($scope, cityService){
+	$scope.city = cityService.city;
 }]);
+
