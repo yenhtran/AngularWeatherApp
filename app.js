e04c87da02bf70999be7a5bfd9b0f70bb5529d25
Yen Tran <yen.tran@autodesk.com>
2017-01-10 09:33:09 -0800

    sets up controllers, routes, pages/

--- app.js
+++ app.js
@@ -1,2 +1,27 @@
 //MODULE
 var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
+
+//ROUTES
+weatherApp.config(function($routeProvider) {
+	$routeProvider
+
+	.when('/', {
+		templateUrl: 'pages/home.html',
+		controller: 'homeController'
+	})
+
+	.when('/forecast', {
+		templateUrl: 'pages/forecast.html',
+		controller: 'forecastController'
+	})
+
+});
+
+//CONTROLLER
+weatherApp.controller('homeController', ['$scope', '$log', function($scope, $log){
+	$scope.name = 'Home';
+}]);
+
+weatherApp.controller('forecastController', ['$scope', function($scope, $log){
+	$scope.name = 'Forecast';
+}]);
