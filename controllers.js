000ded21427d9ae7040233fa45a93c950535fa30
Yen Tran <yen.tran@autodesk.com>
2017-01-11 15:35:52 -0800

    changes 'Get Forecast' link to button

--- controllers.js
+++ controllers.js
@@ -1,8 +1,14 @@
-weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService){
+weatherApp.controller('homeController', ['$scope', '$location', 'cityService', function($scope, $location, cityService){
 	$scope.city = cityService.city;
+                                         
     $scope.$watch('city', function(){
         cityService.city = $scope.city;
-    });                                                                           
+    });      
+    
+    $scope.submit = function() {
+        $location.path('/forecast');
+    };
+                                         
 }]);
 
 weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function($scope, $resource, $routeParams, cityService){
